aa60dd2275a81dfe9f2ce2e5b3406903e43b6cf6
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2024-12-10 08:42:58 -0300

    load model (release not working)

--- android/app/src/main/kotlin/com/example/santorini_classifier/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/santorini_classifier/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.santorini_classifier
 
+import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import org.pytorch.Module
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "pytorch_classifier"
+    private lateinit var module: Module
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -13,12 +19,47 @@ class MainActivity: FlutterActivity() {
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             when (call.method) {
                 "loadModel" -> {
-                    result.success("Loaded")
+                    val modelPath = "classifier.pt"
+                    try {
+                        val filePath = assetFilePath(this, modelPath)
+                        module = Module.load(filePath)
+                        result.success(filePath)
+                    } catch (e: Exception) {
+                        result.error("LOAD_ERROR",modelPath, e)
+                    }
                 }
                 else -> {
                     result.notImplemented()
                 }
             }
         }
+    }
+
+    fun assetFilePath(context: Context, asset: String): String {
+        val file = File(context.filesDir, asset)
+
+        try {
+            val inpStream: InputStream = context.assets.open(asset)
+            try {
+                val outStream = FileOutputStream(file, false)
+                val buffer = ByteArray(4 * 1024)
+                var read: Int
+
+                while (true) {
+                    read = inpStream.read(buffer)
+                    if (read == -1) {
+                        break
+                    }
+                    outStream.write(buffer, 0, read)
+                }
+                outStream.flush()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return file.absolutePath
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return ""
     }
 }
